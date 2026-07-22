5352d6331ea1c8b13595ad354b3e3326263062a6
Irene1-bit <irene.deluca2008@gmail.com>
2026-07-22 07:38:55 +0000

    salvattagio

--- es2.py
+++ es2.py
@@ -33,33 +33,33 @@ Burnt green, and blue and white.
 
 #punt1 
 'conto le righe non vuote'
-righe = testo.split("\n")          #divide il testo in righe usando a capo
-righe_non_vuote = [r for r in righe if r.strip() !=""]      #elimina le righe vuote
-print (len(righe_non_vuote))         #stampa il numero di righe utili
+righe = testo.split("\n")          #divide il testo in righe ogni volta che andiamo a capo
+righe_non_vuote = [r for r in righe if r.strip() !=""]      #elimina le righe vuote usando la list comprehension, orima elimina gli spazi, se non rimane nulla la elimina
+print (len(righe_non_vuote))         #stampa il numero di righe non vuote
  
 input("premi invio per continuare")
 #punto 2 
 'conto le parole'
-parole=testo.split ()       #divide il testo in parole
+parole=testo.split ()       #divide il testo in parole in bae agli spazi
 print(len(parole))
 
 input("premi invio per continuare")
 
 #punt 3 
 'conto i caratteri alfanumerici'
-conta =0
-for c in testo:
+conta =0    #inizializza contatore partendo da zero
+for c in testo:     #assegna ai caratteri la variabile c
     if c.isalnum():     #controlla se è lettera o è numero
-        conta +=1
+        conta +=1       #incrementa il contatore
 print(conta)
 
 input("premi invio per continuare")
 
 #punto4 
 'conto una lettera a scelta'
-lettera=input("dimmi un lettera: ")
-conteggio =0
-for c in testo:
+lettera=input("dimmi un lettera: ")     #chiede una lettera come input
+conteggio =0    #inizializza un contatore da zero
+for c in testo:     
     if c.lower() ==lettera.lower():     #confronta ignorando maiuscole e minoscole
         conteggio+=1
 
@@ -68,8 +68,8 @@ print(conteggio)
 input("premi invio per continuare")
 
 #punto5 
-'sostiuisco con python alcune parole'
-testo_mod =testo.lower()         #porta tutto minuscolo 
+'sostiuisco con PYTHON '
+testo_mod =testo.lower()         #porta tutto minuscolo e salva in una nuova variabile
 testo_mod=testo_mod.replace ("day", "PYTHON")
 testo_mod=testo_mod.replace ("water", "PYTHON")
 testo_mod=testo_mod.replace ("about", "PYTHON")
@@ -86,7 +86,7 @@ for i in range (len(parole)):
     if i % 2==1:        #posizione dispari
         parole [i]=parole[i].upper()
 
-nuovo_testo=" ".join(parole)     #spazio tra le parole
+nuovo_testo=" ".join(parole)     #prende gli elementi da parole e li unisce separandoli con uno spazio
 print(nuovo_testo)
 
 
@@ -96,10 +96,10 @@ input("premi invio per continuare")
 
 #punto7
 'inverto ordine delle righe'
-righe=testo.strip().split("\n")     #rimuove gli spazi iniziali e finali e divide
-righe_invertite=righe[::-1]     #slicing per invertire la lista
+righe=testo.strip().split("\n")     #rimuove gli spazi iniziali e finali e divide ogni volta che va a capo
+righe_invertite=righe[::-1]     #slicing per invertire la lista, utilizzo -1 per invertire
 
-print("\n".join(righe_invertite))       #ricompone il testo
+print("\n".join(righe_invertite))       #ricompone il testo inserendo il a capo
 
 
 input("premi invio per continuare")
@@ -112,13 +112,13 @@ risultato=[]
 
 
 
-for strofa in strofe:
+for strofa in strofe:               #scorre una strofa alla volta
     versi=strofa.split("\n")        #divide la strofa in versi
 
     if len(versi) >1:
-        versi[1]=versi[1][::-1]     #inverte il secondo verso
+        versi[1]=versi[1][::-1]     #inverte il secondo verso, perche parte da zero, e lo inverte
 
-        risultato.append("\n".join(versi))
+        risultato.append("\n".join(versi)) #riunisce tutto insieme e separa con uno spazio, e aggiunge alla lista risultato
 
 print ("\n\n".join(risultato))
 
@@ -133,12 +133,12 @@ insiemi = []
 for strofa in strofe:
     # Rimuoviamo la punteggiatura per evitare che "ocean." o "christ!" sballino il conteggio
     for carattere in [',', '.', ';', ':', '!', '?']:
-        strofa = strofa.replace(carattere, '')
-    parole = set(strofa.split())
-    insiemi.append(parole)
+        strofa = strofa.replace(carattere, '') #lo trasforma in uno spazio
+    parole = set(strofa.split()) #mette in un set che elimina i duplicati
+    insiemi.append(parole) #aggiunge alla fine di insiemi la lista di parole
 
 # Intersezione di tutti i set
-comuni = set.intersection(*insiemi)
+comuni = set.intersection(*insiemi)     #trova gli elementi in intersezione tra tutti gli insiemi 
 
 # Controlliamo se il set 'comuni' contiene qualcosa
 if len(comuni) > 0:
@@ -155,7 +155,7 @@ input("premi invio per continuare")
 #punto 10
 'lista univoca di parole ordinata per lunghezza'
 parole=testo.lower().split()
-uniche=list(set(parole))        #rimuove duplicati
+uniche=list(set(parole))        #rimuove duplicati mettendole nel set
 
 uniche.sort(key=len)        #ordina per lunghezza parola
 print (uniche)
@@ -167,13 +167,13 @@ input("premi invio per continuare")
 
 #punto11
 
-diz= {}
+diz= {}     #crea dizionario vuoto
 
-for c in testo:
-    if c in diz:
+for c in testo:    # scorre il testo e asssegna la variabile 
+    if c in diz:    #se c è gia nel dizionario incrfementa di uno
         diz[c] +=1
     else:
-        diz[c]=1
+        diz[c]=1        #se non c'è le assegna il valore 1
 print(diz)
 
 
@@ -184,7 +184,7 @@ input("premi invio per continuare")
 
 #punto 12
 
-diz2 ={}
+diz2 ={}        #stessa logica del punto prima ma vede che sia carattere, numero
 for c in testo.lower():
     if c.isalnum():
         if c in diz2:
